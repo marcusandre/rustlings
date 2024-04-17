330f2ad7ab07212a1c7f71b2214e06ba53d68030
Marcus André <hello+github@marcusandre.de>
2024-04-17 20:28:43 +0200

    iterators

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -3,9 +3,14 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    (1..num).rfold(1, |acc, n| acc * (n + 1))
+    // match num {
+    //     0 => 1,
+    //     1 => 1,
+    //     _ => factorial(num - 1) * num,
+    // }
+
     // Complete this function to return the factorial of num
     // Do not use:
     // - early returns (using the `return` keyword explicitly)
